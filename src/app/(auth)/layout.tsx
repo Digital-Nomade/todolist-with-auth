028ac580f7a38653d8ec999f7c325c15e0353da8
Bruno Russo <bruno@jumpstartmd.com>
2024-10-09 12:54:29 -0300

    feat: add auth layout

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+export default function RootLayout({children}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="h-[100vh]">
+      <body className='bg-gradient-to-br from-primary-light to-primary-dark h-[100%] w-full p-8'>
+        {children}
+      </body>
+    </html>
+  )
+}
