10bb7a0ee1153b4c65117ee5944e6ee477eb5c20
Bruno Russo <bruno@jumpstartmd.com>
2024-12-05 17:30:09 -0300

    feat: add authentication verification

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,10 +1,22 @@
+'use client'
+
+import { useAppSelector } from '@/lib/hooks';
 import { MotionConfig } from 'framer-motion';
+import { useRouter } from 'next/navigation';
+
 export default function RootLayout({children}: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { isUserAuthenticated } = useAppSelector(state => state.auth)
+  const router = useRouter()
+
+  if (isUserAuthenticated) {
+    return router.push('/home')
+  }
+
   return (
     <html lang="en" className="h-[100vh]">
-      <body className='bg-gradient-to-br from-primary-light to-primary-dark h-[100%] w-full p-8 flex'>
+      <body className='bg-gradient-to-br from-primary-light to-primary-dark h-[100%] w-full p-8 flex overflow-hidden'>
         <MotionConfig transition={{ ease: 'easeInOut'}}>
           {children}
         </MotionConfig>
